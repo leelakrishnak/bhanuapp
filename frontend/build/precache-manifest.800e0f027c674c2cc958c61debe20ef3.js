0a52b9088a235bf20022d43a92be4a4c3020897a
Leelakrishna Kantamneni <leelakrishnak@fb.com>
2020-09-12 22:41:50 -0700

    rearranged dev folders

---
+++ frontend/build/precache-manifest.800e0f027c674c2cc958c61debe20ef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd952df6219719de2d1debf4dff4795",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc4af47c71d369658633",
+    "url": "/static/css/main.4d124e39.chunk.css"
+  },
+  {
+    "revision": "5f33c3eccae08a83172d",
+    "url": "/static/js/2.2c7be511.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.2c7be511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc4af47c71d369658633",
+    "url": "/static/js/main.e2dfc4b8.chunk.js"
+  },
+  {
+    "revision": "5c973a82f9ff51b13122",
+    "url": "/static/js/runtime-main.8376cb16.js"
+  }
+]);
